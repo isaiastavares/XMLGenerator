870ae658f4cabaf23ce7780f7770462c24c7edab
Koziel101 <hiagokoziel100@gmail.com>
2016-11-19 18:53:41 -0200

    Atualizando classe XML de informação de endereco para OTC

--- src/main/java/br/ufg/inf/generatorOTC/InfoEndereco.java
+++ src/main/java/br/ufg/inf/generatorOTC/InfoEndereco.java
@@ -5,39 +5,83 @@ import org.simpleframework.xml.Element;
 import org.simpleframework.xml.Root;
 
 @Root(name = "Contato")
-public class InfoOTC extends LaudoBase {
+public class InfoEndereco extends LaudoBase {
 
-    @Element(name = "Razao Social", required = true)
-    private String razaoSocial;
+    @Element(name = "Rua", required = true)
+    private String rua;
 
-    @Element(name = "Cnpj", required = true)
-    private String cnpj;
+    @Element(name = "Numero", required = true)
+    private String numero;
 
-    @Element(name = "Inscricao Social", required = true)
-    private String inscSocial;
+    @Element(name = "Complemento", required = true)
+    private String complemento;
+    
+    @Element(name = "Bairro", required = true)
+    private String bairro;
+    
+    @Element(name = "Municipio", required = true)
+    private String municipio;
+    
+    @Element(name = "UF", required = true)
+    private String uf;
+    
+    @Element(name = "Cep", required = true)
+    private String cep;
 
-    public String getRazaoSocial() {
-        return razaoSocial;
+    public String getRua() {
+        return rua;
     }
 
-    public void setRazaoSocial(String razaoSocial) {
-        this.razaoSocial = razaoSocial;
+    public void setRua(String rua) {
+        this.rua = rua;
     }
 
-    public String getCnpj() {
-        return cnpj;
+    public String getNumero() {
+        return numero;
     }
 
-    public void setCnpj(String cnpj) {
-        this.cnpj = cnpj;
+    public void setNumero(String numero) {
+        this.numero = numero;
     }
 
-    public String getInscSocial() {
-        return inscSocial;
+    public String getComplemento() {
+        return complemento;
     }
 
-    public void setInscSocial(String inscSocial) {
-        this.inscSocial = inscSocial;
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
     }
 
 }
