60dcb2375a6446ff0eab78655585d5ac9e257374
Leonardo Kalyn <Leonardo.Kalyn@gmail.com>
2016-12-01 19:50:48 -0200

    Estrutura de Classes RoteiroAnalise
    Classe refatorada e testes criados

--- src/main/java/br/ufg/inf/generator/xml/RoteiroAnalise.java
+++ src/main/java/br/ufg/inf/generator/xml/RoteiroAnalise.java
@@ -9,33 +9,44 @@ public class RoteiroAnalise extends LaudoBase {
     @Element(name = "VersaoRoteiro", required = true)
     private String versaoRoteiro;
 
+    @Element(name = "VersaoErPaf", required = true)
+    private String versaoErPaf;
+
     @Element(name = "Mes", required = true)
     private String mes;
 
     @Element(name = "Ano", required = true)
     private String ano;
 
-	public String getVersaoRoteiro() {
-		return versaoRoteiro;
-	}
+    public String getVersaoRoteiro() {
+        return versaoRoteiro;
+    }
 
-	public void setVersaoRoteiro(String versaoRoteiro) {
-		this.versaoRoteiro = versaoRoteiro;
-	}
+    public void setVersaoRoteiro(String versaoRoteiro) {
+        this.versaoRoteiro = versaoRoteiro;
+    }
 
-	public String getMes() {
-		return mes;
-	}
+    public String getVersaoErPaf() {
+        return versaoErPaf;
+    }
 
-	public void setMes(String mes) {
-		this.mes = mes;
-	}
+    public void setVersaoErPaf(String versaoErPaf) {
+        this.versaoErPaf = versaoErPaf;
+    }
 
-	public String getAno() {
-		return ano;
-	}
+    public String getMes() {
+        return mes;
+    }
 
-	public void setAno(String ano) {
-		this.ano = ano;
-	}
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
 }
---
+++ src/test/java/br/ufg/inf/generator/xml/RoteiroAnaliseTest.java
@@ -0,0 +1,88 @@
+package br.ufg.inf.generator.xml;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Kalyn
+ */
+public class RoteiroAnaliseTest {
+    private String VERSAOROTEIRO;
+    private String VERSAOERPAF;
+    private String MES;
+    private String ANO;
+    
+    @Test
+    public void testVersaoRoteiroValido() {
+        RoteiroAnalise instance = new RoteiroAnalise();
+        instance.setVersaoRoteiro(VERSAOROTEIRO);
+        String result = instance.getVersaoRoteiro();
+        assertEquals(VERSAOROTEIRO, result);
+    }
+    
+    @Test
+    public void testVersaoErPafValido() {
+        RoteiroAnalise instance = new RoteiroAnalise();
+        instance.setVersaoErPaf(VERSAOERPAF);
+        String result = instance.getVersaoErPaf();
+        assertEquals(VERSAOERPAF, result);
+    }
+
+    @Test
+    public void testMesValido() {
+        RoteiroAnalise instance = new RoteiroAnalise();
+        instance.setMes(MES);
+        String result = instance.getMes();
+        assertEquals(MES, result);
+    }
+
+    @Test
+    public void testAnoValido() {
+        RoteiroAnalise instance = new RoteiroAnalise();
+        instance.setAno(ANO);
+        String result = instance.getAno();
+        assertEquals(ANO, result);
+    }
+    
+    @Test(expected = IllegalStateException.class)
+    public void testVersaoRoteiroNulo(){
+        RoteiroAnalise instance = new RoteiroAnalise();
+        instance.setVersaoRoteiro(null);
+        instance.setVersaoErPaf(VERSAOERPAF);
+        instance.setMes(MES);
+        instance.setAno(ANO);
+        instance.toString();
+    }
+    
+    @Test(expected = IllegalStateException.class)
+    public void testVersaoErPafNulo(){
+        RoteiroAnalise instance = new RoteiroAnalise();
+        instance.setVersaoRoteiro(VERSAOROTEIRO);
+        instance.setVersaoErPaf(null);
+        instance.setMes(MES);
+        instance.setAno(ANO);
+        instance.toString();
+    }
+    
+    @Test(expected = IllegalStateException.class)
+    public void testMesNulo(){
+        RoteiroAnalise instance = new RoteiroAnalise();
+        instance.setVersaoRoteiro(VERSAOROTEIRO);
+        instance.setVersaoErPaf(VERSAOERPAF);
+        instance.setMes(null);
+        instance.setAno(ANO);
+        instance.toString();
+    }
+    
+    @Test(expected = IllegalStateException.class)
+    public void testAnoNulo(){
+        RoteiroAnalise instance = new RoteiroAnalise();
+        instance.setVersaoRoteiro(VERSAOROTEIRO);
+        instance.setVersaoErPaf(VERSAOERPAF);
+        instance.setMes(MES);
+        instance.setAno(null);
+        instance.toString();
+    }
+    
+}
