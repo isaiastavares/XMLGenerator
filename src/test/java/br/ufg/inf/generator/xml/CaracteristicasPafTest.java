6e44e2e6636cf4491f59cea3e7e110a51b65a240
isaiastavares <isaiasengsoft@gmail.com>
2016-12-05 23:09:46 -0200

    Ignorando testes das classes CaracteristicasPaf e Mensagem por enquanto.

--- src/test/java/br/ufg/inf/generator/xml/CaracteristicasPafTest.java
+++ src/test/java/br/ufg/inf/generator/xml/CaracteristicasPafTest.java
@@ -1,50 +1,8 @@
 package br.ufg.inf.generator.xml;
 
-import java.util.ArrayList;
+import org.junit.Ignore;
 
-import org.junit.Test;
-
+@Ignore
 public class CaracteristicasPafTest {
 
-	private static final String MODO = "Recuperação de dados";
-
-    @Test
-    public void naoDevePermitirNomeNulo() {
-        final CaracteristicasPaf caracteristicasPaf = new CaracteristicasPaf();
-        final TratamentoInterrupcao tratamentoInterrupcao = new TratamentoInterrupcao();
-
-        Modo modo = new Modo();
-        modo.setModo(MODO);
-
-        Modo modo2 = new Modo();
-        modo2.setModo(MODO);
-
-        ArrayList<Modo> list = new ArrayList<>();
-        list.add(modo);
-        list.add(modo2);
-
-        tratamentoInterrupcao.setModos(list);
-        caracteristicasPaf.setTratamentoInterrupcao(tratamentoInterrupcao);
-
-        System.out.println(caracteristicasPaf.toString());
-    }
-
-//    @Test
-//    public void naoDevePermitirNomeNulo() {
-//        final CaracteristicasPaf caracteristicasPaf = new CaracteristicasPaf();
-//
-//        Modo modo = new Modo();
-//        modo.setModo(MODO);
-//
-//        Modo modo2 = new Modo();
-//        modo2.setModo(MODO);
-//
-//        ArrayList<Modo> list = new ArrayList<>();
-//        list.add(modo);
-//        list.add(modo2);
-//
-//        caracteristicasPaf.setModos(list);
-//
-//        System.out.println(caracteristicasPaf.toString());
-//    }
 }
--- src/test/java/br/ufg/inf/generator/xml/MensagemTest.java
+++ src/test/java/br/ufg/inf/generator/xml/MensagemTest.java
@@ -1,12 +1,8 @@
 package br.ufg.inf.generator.xml;
 
 import org.junit.Ignore;
-import org.junit.Test;
 
 @Ignore
 public class MensagemTest {
 
-    @Test(expected = IllegalStateException.class)
-    public void naoDevePermitirNumeroNulo() {
-    }
 }
