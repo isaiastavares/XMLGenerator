5f33e2e3889aa02aee1f7dfee06e4fb97d907e26
Matheus Cardoso Duarte Santos <mtesedd@hotmail.com>
2016-12-09 16:47:22 -0200

    Refatorando testes CaracteristicasPafTest

--- src/test/java/br/ufg/inf/generator/xml/CaracteristicasPafTest.java
+++ src/test/java/br/ufg/inf/generator/xml/CaracteristicasPafTest.java
@@ -3,6 +3,7 @@ package br.ufg.inf.generator.xml;
 import java.util.ArrayList;
 import java.util.List;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.fail;
 import org.junit.Test;
 
@@ -15,8 +16,9 @@ public class CaracteristicasPafTest {
      */
     @Test
     public void testGetLinguagemProgramacao() {
-        carac.setLinguagemProgramacao("Java");
-        assertEquals("java", carac.getLinguagemProgramacao());
+        String linguagemProgramacao = "Java";
+        carac.setLinguagemProgramacao(linguagemProgramacao);
+        assertEquals(linguagemProgramacao, carac.getLinguagemProgramacao());
     }
 
     /**
@@ -24,12 +26,9 @@ public class CaracteristicasPafTest {
      */
     @Test
     public void testSetLinguagemProgramacao() {
-        System.out.println("setLinguagemProgramacao");
-        String linguagemProgramacao = "";
-        CaracteristicasPaf instance = new CaracteristicasPaf();
-        instance.setLinguagemProgramacao(linguagemProgramacao);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        String linguagemProgramacao = "C";
+        carac.setLinguagemProgramacao(linguagemProgramacao);
+        assertEquals(linguagemProgramacao, carac.getLinguagemProgramacao());
     }
 
     /**
@@ -37,13 +36,9 @@ public class CaracteristicasPafTest {
      */
     @Test
     public void testGetSistemaOperacional() {
-        System.out.println("getSistemaOperacional");
-        CaracteristicasPaf instance = new CaracteristicasPaf();
-        String expResult = "";
-        String result = instance.getSistemaOperacional();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        String sistemaOperacional = "Windows";
+        carac.setSistemaOperacional(sistemaOperacional);
+        assertEquals(sistemaOperacional, carac.getSistemaOperacional());
     }
 
     /**
@@ -51,12 +46,9 @@ public class CaracteristicasPafTest {
      */
     @Test
     public void testSetSistemaOperacional() {
-        System.out.println("setSistemaOperacional");
-        String sistemaOperacional = "";
-        CaracteristicasPaf instance = new CaracteristicasPaf();
-        instance.setSistemaOperacional(sistemaOperacional);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        String sistemaOperacional = "MAC";
+        carac.setSistemaOperacional(sistemaOperacional);
+        assertEquals(sistemaOperacional, carac.getSistemaOperacional());
     }
 
     /**
@@ -64,13 +56,12 @@ public class CaracteristicasPafTest {
      */
     @Test
     public void testGetGerenciadorBancoDados() {
-        System.out.println("getGerenciadorBancoDados");
-        CaracteristicasPaf instance = new CaracteristicasPaf();
-        List<String> expResult = null;
-        List<String> result = instance.getGerenciadorBancoDados();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        List<String> gerenciadorBancoDados = new ArrayList();
+        gerenciadorBancoDados.add("teste1");
+        gerenciadorBancoDados.add("teste2");
+        gerenciadorBancoDados.add("teste3");
+        carac.setGerenciadorBancoDados(gerenciadorBancoDados);
+        assertEquals(gerenciadorBancoDados, carac.getGerenciadorBancoDados());
     }
 
     /**
@@ -78,12 +69,12 @@ public class CaracteristicasPafTest {
      */
     @Test
     public void testSetGerenciadorBancoDados() {
-        System.out.println("setGerenciadorBancoDados");
-        List<String> gerenciadorBancoDados = null;
-        CaracteristicasPaf instance = new CaracteristicasPaf();
-        instance.setGerenciadorBancoDados(gerenciadorBancoDados);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        List<String> gerenciadorBancoDados = new ArrayList();
+        gerenciadorBancoDados.add("teste3");
+        gerenciadorBancoDados.add("teste2");
+        gerenciadorBancoDados.add("teste1");
+        carac.setGerenciadorBancoDados(gerenciadorBancoDados);
+        assertEquals(gerenciadorBancoDados, carac.getGerenciadorBancoDados());
     }
 
     /**
@@ -91,13 +82,9 @@ public class CaracteristicasPafTest {
      */
     @Test
     public void testGetTipoDesenvolvimento() {
-        System.out.println("getTipoDesenvolvimento");
-        CaracteristicasPaf instance = new CaracteristicasPaf();
-        String expResult = "";
-        String result = instance.getTipoDesenvolvimento();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        String tipoDesenvolvimento = "Tipo1";
+        carac.setTipoDesenvolvimento(tipoDesenvolvimento);
+        assertEquals(tipoDesenvolvimento, carac.getTipoDesenvolvimento());
     }
 
     /**
@@ -105,12 +92,9 @@ public class CaracteristicasPafTest {
      */
     @Test
     public void testSetTipoDesenvolvimento() {
-        System.out.println("setTipoDesenvolvimento");
-        String tipoDesenvolvimento = "";
-        CaracteristicasPaf instance = new CaracteristicasPaf();
-        instance.setTipoDesenvolvimento(tipoDesenvolvimento);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        String tipoDesenvolvimento = "Tipo2";
+        carac.setTipoDesenvolvimento(tipoDesenvolvimento);
+        assertEquals(tipoDesenvolvimento, carac.getTipoDesenvolvimento());
     }
 
     /**
@@ -118,13 +102,9 @@ public class CaracteristicasPafTest {
      */
     @Test
     public void testGetTipoFuncionamento() {
-        System.out.println("getTipoFuncionamento");
-        CaracteristicasPaf instance = new CaracteristicasPaf();
-        String expResult = "";
-        String result = instance.getTipoFuncionamento();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        String tipoFuncionamento = "Tipo1";
+        carac.setTipoFuncionamento(tipoFuncionamento);
+        assertEquals(tipoFuncionamento, carac.getTipoFuncionamento());
     }
 
     /**
@@ -132,12 +112,9 @@ public class CaracteristicasPafTest {
      */
     @Test
     public void testSetTipoFuncionamento() {
-        System.out.println("setTipoFuncionamento");
-        String tipoFuncionamento = "";
-        CaracteristicasPaf instance = new CaracteristicasPaf();
-        instance.setTipoFuncionamento(tipoFuncionamento);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        String tipoFuncionamento = "Tipo2";
+        carac.setTipoFuncionamento(tipoFuncionamento);
+        assertEquals(tipoFuncionamento, carac.getTipoFuncionamento());
     }
 
     /**
@@ -145,13 +122,11 @@ public class CaracteristicasPafTest {
      */
     @Test
     public void testGetMeioGeracaoArquivoStintegraEfd() {
-        System.out.println("getMeioGeracaoArquivoStintegraEfd");
-        CaracteristicasPaf instance = new CaracteristicasPaf();
-        ArrayList<Modo> expResult = null;
-        ArrayList<Modo> result = instance.getMeioGeracaoArquivoStintegraEfd();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Modo modo = new Modo();
+        ArrayList<Modo> listModo = new ArrayList();
+        listModo.add(modo);
+        carac.setMeioGeracaoArquivoStintegraEfd(listModo);
+        assertEquals(listModo, carac.getMeioGeracaoArquivoStintegraEfd());
     }
 
     /**
@@ -159,12 +134,11 @@ public class CaracteristicasPafTest {
      */
     @Test
     public void testSetMeioGeracaoArquivoStintegraEfd() {
-        System.out.println("setMeioGeracaoArquivoStintegraEfd");
-        ArrayList<Modo> meioGeracaoArquivoStintegraEfd = null;
-        CaracteristicasPaf instance = new CaracteristicasPaf();
-        instance.setMeioGeracaoArquivoStintegraEfd(meioGeracaoArquivoStintegraEfd);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Modo modo = new Modo();
+        ArrayList<Modo> listModo = new ArrayList();
+        listModo.add(modo);
+        carac.setMeioGeracaoArquivoStintegraEfd(listModo);
+        assertEquals(listModo, carac.getMeioGeracaoArquivoStintegraEfd());
     }
 
     /**
@@ -172,13 +146,9 @@ public class CaracteristicasPafTest {
      */
     @Test
     public void testGetIntegracaoPaf() {
-        System.out.println("getIntegracaoPaf");
-        CaracteristicasPaf instance = new CaracteristicasPaf();
-        String expResult = "";
-        String result = instance.getIntegracaoPaf();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        String integracaoPaf = "IntegraçãoPaf";
+        carac.setIntegracaoPaf(integracaoPaf);
+        assertEquals(integracaoPaf, carac.getIntegracaoPaf());
     }
 
     /**
@@ -186,12 +156,9 @@ public class CaracteristicasPafTest {
      */
     @Test
     public void testSetIntegracaoPaf() {
-        System.out.println("setIntegracaoPaf");
-        String integracaoPaf = "";
-        CaracteristicasPaf instance = new CaracteristicasPaf();
-        instance.setIntegracaoPaf(integracaoPaf);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        String integracaoPaf = "IntegraçãoPaf";
+        carac.setIntegracaoPaf(integracaoPaf);
+        assertEquals(integracaoPaf, carac.getIntegracaoPaf());
     }
 
     /**
@@ -199,13 +166,11 @@ public class CaracteristicasPafTest {
      */
     @Test
     public void testGetFormaImpressao() {
-        System.out.println("getFormaImpressao");
-        CaracteristicasPaf instance = new CaracteristicasPaf();
-        ArrayList<Modo> expResult = null;
-        ArrayList<Modo> result = instance.getFormaImpressao();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Modo modo = new Modo();
+        ArrayList<Modo> formaImpressão = new ArrayList();
+        formaImpressão.add(modo);
+        carac.setFormaImpressao(formaImpressão);
+        assertEquals(formaImpressão, carac.getFormaImpressao());
     }
 
     /**
@@ -213,12 +178,11 @@ public class CaracteristicasPafTest {
      */
     @Test
     public void testSetFormaImpressao() {
-        System.out.println("setFormaImpressao");
-        ArrayList<Modo> formaImpressao = null;
-        CaracteristicasPaf instance = new CaracteristicasPaf();
-        instance.setFormaImpressao(formaImpressao);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Modo modo = new Modo();
+        ArrayList<Modo> formaImpressão = new ArrayList();
+        formaImpressão.add(modo);
+        carac.setFormaImpressao(formaImpressão);
+        assertEquals(formaImpressão, carac.getFormaImpressao());
     }
 
     /**
@@ -226,13 +190,11 @@ public class CaracteristicasPafTest {
      */
     @Test
     public void testGetTratamentoInterrupcao() {
-        System.out.println("getTratamentoInterrupcao");
-        CaracteristicasPaf instance = new CaracteristicasPaf();
-        ArrayList<Modo> expResult = null;
-        ArrayList<Modo> result = instance.getTratamentoInterrupcao();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Modo modo = new Modo();
+        ArrayList<Modo> tratamentoInterrupcao = new ArrayList();
+        tratamentoInterrupcao.add(modo);
+        carac.setTratamentoInterrupcao(tratamentoInterrupcao);
+        assertEquals(tratamentoInterrupcao, carac.getTratamentoInterrupcao());
     }
 
     /**
@@ -240,12 +202,11 @@ public class CaracteristicasPafTest {
      */
     @Test
     public void testSetTratamentoInterrupcao() {
-        System.out.println("setTratamentoInterrupcao");
-        ArrayList<Modo> tratamentoInterrupcao = null;
-        CaracteristicasPaf instance = new CaracteristicasPaf();
-        instance.setTratamentoInterrupcao(tratamentoInterrupcao);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Modo modo = new Modo();
+        ArrayList<Modo> tratamentoInterrupcao = new ArrayList();
+        tratamentoInterrupcao.add(modo);
+        carac.setTratamentoInterrupcao(tratamentoInterrupcao);
+        assertEquals(tratamentoInterrupcao, carac.getTratamentoInterrupcao());
     }
 
     /**
@@ -253,13 +214,11 @@ public class CaracteristicasPafTest {
      */
     @Test
     public void testGetAplicacoesEspeciais() {
-        System.out.println("getAplicacoesEspeciais");
-        CaracteristicasPaf instance = new CaracteristicasPaf();
-        ArrayList<AplicacaoEspecial> expResult = null;
-        ArrayList<AplicacaoEspecial> result = instance.getAplicacoesEspeciais();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        AplicacaoEspecial aplicacaoEspecial = new AplicacaoEspecial();
+        ArrayList<AplicacaoEspecial> aplicacoesEspeciais = new ArrayList();
+        aplicacoesEspeciais.add(aplicacaoEspecial);
+        carac.setAplicacoesEspeciais(aplicacoesEspeciais);
+        assertEquals(aplicacoesEspeciais, carac.getAplicacoesEspeciais());
     }
 
     /**
@@ -267,12 +226,11 @@ public class CaracteristicasPafTest {
      */
     @Test
     public void testSetAplicacoesEspeciais() {
-        System.out.println("setAplicacoesEspeciais");
-        ArrayList<AplicacaoEspecial> aplicacoesEspeciais = null;
-        CaracteristicasPaf instance = new CaracteristicasPaf();
-        instance.setAplicacoesEspeciais(aplicacoesEspeciais);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        AplicacaoEspecial aplicacaoEspecial = new AplicacaoEspecial();
+        ArrayList<AplicacaoEspecial> aplicacoesEspeciais = new ArrayList();
+        aplicacoesEspeciais.add(aplicacaoEspecial);
+        carac.setAplicacoesEspeciais(aplicacoesEspeciais);
+        assertEquals(aplicacoesEspeciais, carac.getAplicacoesEspeciais());
     }
 
 }
