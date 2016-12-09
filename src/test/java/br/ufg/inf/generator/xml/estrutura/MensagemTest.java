56777dad5cd302d4f5cb60e39cb1cd508c95413c
Leonardo Kalyn <Leonardo.Kalyn@gmail.com>
2016-12-09 19:23:57 -0200

    Refatoração Pacote de Teste Xml.Estrutura

--- src/test/java/br/ufg/inf/generator/xml/estrutura/MensagemTest.java
+++ src/test/java/br/ufg/inf/generator/xml/estrutura/MensagemTest.java
@@ -1,4 +1,4 @@
-package br.ufg.inf.generator.xml;
+package br.ufg.inf.generator.xml.estrutura;
 
 import br.ufg.inf.generator.xml.estrutura.Mensagem;
 import br.ufg.inf.generator.xml.estrutura.mensagem.TestadorAprovacaoRelatorio;
