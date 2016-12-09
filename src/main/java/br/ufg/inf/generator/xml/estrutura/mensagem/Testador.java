dfbc2ede3ec194a674e60c7cc4ad6f6e8572ff0a
Matheus Cardoso Duarte Santos <mtesedd@hotmail.com>
2016-12-09 19:25:24 -0200

    Refatorando classes TestadorAprocacao e TestadorExecucao

--- src/main/java/br/ufg/inf/generator/xml/estrutura/mensagem/Testador.java
+++ src/main/java/br/ufg/inf/generator/xml/estrutura/mensagem/Testador.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.ufg.inf.generator.xml.estrutura.mensagem;
 
-/**
- *
- * @author Matheus
- */
-public class Testador {
+import br.ufg.inf.generator.xml.estrutura.LaudoBase;
+import org.simpleframework.xml.Element;
+
+public class Testador extends LaudoBase{
     
+    @Element(name = "Nome", required = true)
+    private String nome;
+
+    @Element(name = "Cargo", required = true)
+    private String cargo;
+
+    @Element(name = "Cpf", required = true)
+    private String cpf;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
